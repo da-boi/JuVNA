9de341201d1d78662cfd6d7b90ccddc21e8996fc
Dominik Bergermann <dominik.bergermann@gmx.de>
2023-04-13 21:17:08 +0200

    init

--- src/libximc_api.jl
+++ src/libximc_api.jl
@@ -21,8 +21,8 @@ import Base.Libc.Libdl: dlopen, dlclose, dlsym
 # lib = loadDLL()
 
 # const libximc = dlopen(joinpath("XIMC","ximc-2.13.6","ximc","win64","libximc"))
-# const libximc = dlopen(joinpath("src","win64","libximc"))
-const libximc = dlopen("libximc")
+const libximc = dlopen(joinpath("src","win64","libximc"))
+# const libximc = dlopen(joinpath("src","libximc"))
 
 function set_feedback_settings(id, feedback_settings)
     ccall((:set_feedback_settings, :libximc), result_t, (device_t, Ptr{feedback_settings_t}), id, feedback_settings)
