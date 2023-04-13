d5c29a6973b885ce931ce40fe0b6b9a8ff8a5a8a
Dominik Bergermann <dominik.bergermann@gmx.de>
2023-04-13 20:46:37 +0200

    init

--- src/JuXIMC.jl
+++ src/JuXIMC.jl
@@ -236,27 +236,27 @@ end
 
 
 
-function commandWaitForStop(device::DeviceId; interval::UInt32=0x00000a,info::Bool=false)
-    info && println("\nWaiting for stop")
+# function commandWaitForStop(device::DeviceId; interval::UInt32=0x00000a,info::Bool=false)
+#     info && println("\nWaiting for stop")
 
-    result = command_wait_for_stop(device,interval)
+#     result = command_wait_for_stop(device,interval)
 
-    info && println("\nResult: "*StringView(result))
+#     info && println("\nResult: "*StringView(result))
 
-    return result
-end
+#     return result
+# end
 
-const commandWaitForStop(device::DeviceId; interval::Integer=10,info::Bool=false) = commandWaitForStop(device; interval=UInt32(interval),info=info)
-const commandWaitForStop(device::DeviceId; interval::Real=10,info::Bool=false) = commandWaitForStop(device; interval=UInt32(interval),info=info)
+# const commandWaitForStop(device::DeviceId; interval::Integer=10,info::Bool=false) = commandWaitForStop(device; interval=UInt32(interval),info=info)
+# const commandWaitForStop(device::DeviceId; interval::Real=10,info::Bool=false) = commandWaitForStop(device; interval=UInt32(interval),info=info)
 
-function commandWaitForStop(devices::Vector{DeviceId}; interval::UInt32=0x00000a,info::Bool=false)
-    for i in eachindex(devices)
-        commandWaitForStop(devices[i]; interval=interval,info=info)
-    end
-end
+# function commandWaitForStop(devices::Vector{DeviceId}; interval::UInt32=0x00000a,info::Bool=false)
+#     for i in eachindex(devices)
+#         commandWaitForStop(devices[i]; interval=interval,info=info)
+#     end
+# end
 
-const commandWaitForStop(devices::DeviceId; interval::Integer=10,info::Bool=false) = commandWaitForStop(devices; interval=UInt32(interval),info=info)
-const commandWaitForStop(devices::DeviceId; interval::Real=10,info::Bool=false) = commandWaitForStop(devices; interval=UInt32(interval),info=info)
+# const commandWaitForStop(devices::DeviceId; interval::Integer=10,info::Bool=false) = commandWaitForStop(devices; interval=UInt32(interval),info=info)
+# const commandWaitForStop(devices::DeviceId; interval::Real=10,info::Bool=false) = commandWaitForStop(devices; interval=UInt32(interval),info=info)
 
 function enumerateDevices(flags,hints::Union{Base.CodeUnits{UInt8, String},String})
     hints = Vector{UInt8}(hints)
