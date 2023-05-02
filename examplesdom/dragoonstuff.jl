601537020f78a49d3a3206c7022d23b471f4d9d0
Dominik Bergermann <dominik.bergermann@gmx.de>
2023-05-02 18:01:02 +0200

    first implementation collision check

--- examplesdom/dragoonstuff.jl
+++ examplesdom/dragoonstuff.jl
@@ -1,26 +1,111 @@
 
+mutable struct Boundaries <: Dragoon.BoundariesType
+    lo::Float64
+    hi::Float64
+
+    function Boundaries()
+        new(0,0)
+    end
+
+    function Boundaries(hi,lo)
+        new(hi,lo)
+    end
+end
+
+function checkBoundaries(b::Boundaries)
+    if b.hi < b.lo
+        error("Boundary failure: hi < lo!")
+    end
+end
+
+
+
 
 
 mutable struct Devices <: DevicesType
     ids::Vector{DeviceId}
 
     stagenames::Vector{String}
-    stagecals::Vector{Tuple{Int,Symbol}}
-    stagecolls::Vector{Tuple{Float64,}}
-    stagezeros::Vector{Tuple{Float64,Float64}}
-    stageborders::Vector{Tuple{Float64,Float64}}
+    stagecals::Vector{Tuple{Symbol,Float64}}
+    stagecols::Vector{Tuple{Symbol,Float64,Float64}}
+    stagezeros::Vector{Tuple{Symbol,Float64}}
+    stageborders::Vector{Tuple{Symbol,Float64,Float64}}
 
-    function Devices(ids,stagecals::Dict,stagecolls::Dict,stagezeros::Dict,stageborders::Dict)
+    function Devices(ids,stagecals::Dict,stagecols::Dict,stagezeros::Dict,stageborders::Dict)
         sn = [getStageName(D[i]) for i in eachindex(D)]
         scal = [stagecals[getStageName(D[i])] for i in eachindex(D)]
         sz = [stagezeros[getStageName(D[i])] for i in eachindex(D)]
-        scol = [stagecolls[getStageName(D[i])] for i in eachindex(D)]
+        scol = [stagecols[getStageName(D[i])] for i in eachindex(D)]
         sb = [stageborders[getStageName(D[i])] for i in eachindex(D)]
-
 
         new(ids,sn,scal,sz,scol,sb)
     end
 end
+
+
+
+function pos2steps(pos::Float64,
+        stagecal::Tuple{Symbol,Float64},
+        stagezero::Tuple{Symbol,Float64},;
+        inputunit=:m)
+
+    return x2steps(pos/inputunit-stagezero[2]/stagezero[1];
+        inputunit=:m,cal=stagecal)
+end
+
+function pos2steps(booster::PhysicalBooster; inputunit=:m)
+    s = Array{Int}(undef,booster.ndisk)
+
+    for i in 1:booster.ndisk
+        s[i] = pos2steps(booster.pos[i],booster.devices.stagecals[i],
+            booster.devices.stagezeros[i]; inputunit=inputunit)
+    end
+
+    return s
+end
+
+function pos2steps(booster::PhysicalBooster; inputunit=:m)
+    return @. pos2steps(booster.pos,booster.devices.stagecals,
+        booster.devices.stagezeros; inputunit=inputunit)
+end
+
+
+
+function steps2pos(steps::Int,
+        stagecal::Tuple{Symbol,Float64},
+        stagezero::Tuple{Symbol,Float64},;
+        outputunit=:m)
+    
+    return steps2x(steps; outputunit=outputunit,cal=stagecal) +
+        stagezero[2]*stagezero[1]/outputunit
+end
+
+
+
+
+function checkCollision(pos::Vector{Float64},
+        booster::PhysicalBooster)
+    
+    for i in 1:booster.ndisk-1
+        if pos[i]-pos[i+1] < 0
+            return true
+        elseif pos[i]-pos[i+1] < booster.devices.stageborders[i][3]-
+                booster.devices.stageborders[i+1][2]
+            return true
+        end
+    end            
+end
+
+function checkCollision(pos::Vector{Float64},newpos::Vector{Float64},
+        booster::PhysicalBooster; steps::Int=10)
+    
+    for i in 1:steps
+        if checkCollision(pos+(newpos-pos)*i/steps,booster)
+            return true
+        end
+    end
+end
+
 
 
 
@@ -40,7 +125,7 @@ end
 
 function move(booster::PhysicalBooster,newpos::Vector{Float64}; additive=false)
     if additive
-        checkCollision(booster.pos,booster.pos+newpos) && error("Discs are about to collide!")
+        checkCollision(booster.pos+newpos,booster) && error("Discs are about to collide!")
         
         commandMove(devices::Vector{DeviceId},x::Vector{<:Real},cal::Dict{String,Tuple{Int,Symbol}}; info=false,inputunit=:mm)
 
@@ -49,7 +134,7 @@ function move(booster::PhysicalBooster,newpos::Vector{Float64}; additive=false)
             inputunit=:m)
         commandWaitForStop(booster.devices.id)
     else
-        checkCollision(booster.pos,newpos) && error("Discs are about to collide!")
+        checkCollision(newpos,booster) && error("Discs are about to collide!")
 
         booster.pos = copy(newpos)
         
