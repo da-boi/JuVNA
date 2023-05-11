0dc1396dc9d6355c1b29e513476f7d25c1c71bba
Dominik Bergermann <dominik.bergermann@gmx.de>
2023-05-12 00:08:29 +0200

    dragoon integration stuff

--- examplesdom/testdragoon.jl
+++ examplesdom/testdragoon.jl
@@ -45,16 +45,29 @@ freqsplot = genFreqs(22.025e9,150e6; length=1000)
 
 hist = initHist(booster,10000,freqs,(getObjAna1d,[]))
 
-nelderMead(booster::AnalyticalBooster,hist::Vector{State},freqs::Array{Float64},
-    α::Float64,β::Float64,γ::Float64,δ::Float64,
-    objFunction::Tuple{Function,Vector},
-    initSimplex::Tuple{Function,Vector},
-    simplexObj::Tuple{Function,Vector},
-    unstuckinator::Tuple{Function,Vector};
+# nelderMead(booster::AnalyticalBooster,hist::Vector{State},freqs::Array{Float64},
+#     α::Float64,β::Float64,γ::Float64,δ::Float64,
+#     objFunction::Tuple{Function,Vector},
+#     initSimplex::Tuple{Function,Vector},
+#     simplexObj::Tuple{Function,Vector},
+#     unstuckinator::Tuple{Function,Vector};
 
-    maxiter::Integer=Int(1e2),
-    showtrace::Bool=false,
-    showevery::Integer=1,
-    unstuckisiter::Bool=true,
-    tracecentroid::Bool=false,
-    traceevery::Int=1)
+#     maxiter::Integer=Int(1e2),
+#     showtrace::Bool=false,
+#     showevery::Integer=1,
+#     unstuckisiter::Bool=true,
+#     tracecentroid::Bool=false,
+#     traceevery::Int=1)
+
+    
+trace = nelderMead(b,hist,freqs,
+                    1.,1+2/booster.ndisk,
+                    0.75-1/(2*booster.ndisk),1-1/(booster.ndisk),
+                    (getObjAna1d,[]),
+                    (initSimplexCoord,[1e-4]),
+                    (getSimplexObj,[]),
+                    (unstuckDont,[]);
+                    maxiter=Int(1e3),
+                    showtrace=true,
+                    showevery=100,
+                    unstuckisiter=true);
