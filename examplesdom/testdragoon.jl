47a8d77737f1b34ed593814f5bf9fe1beeed6e02
bergermann <dominik.bergermann@gmx.de>
2023-05-11 22:34:13 +0200

    dragoon integration stuff

--- examplesdom/testdragoon.jl
+++ examplesdom/testdragoon.jl
@@ -40,4 +40,21 @@ homeZero(b)
 
 move(b,[0.01,0.01]; additive=true)
 
-commandMove(D,[10,10],stagecals)
+freqs = genFreqs(22.025e9,50e6; length=10)
+freqsplot = genFreqs(22.025e9,150e6; length=1000)
+
+hist = initHist(booster,10000,freqs,(getObjAna1d,[]))
+
+nelderMead(booster::AnalyticalBooster,hist::Vector{State},freqs::Array{Float64},
+    α::Float64,β::Float64,γ::Float64,δ::Float64,
+    objFunction::Tuple{Function,Vector},
+    initSimplex::Tuple{Function,Vector},
+    simplexObj::Tuple{Function,Vector},
+    unstuckinator::Tuple{Function,Vector};
+
+    maxiter::Integer=Int(1e2),
+    showtrace::Bool=false,
+    showevery::Integer=1,
+    unstuckisiter::Bool=true,
+    tracecentroid::Bool=false,
+    traceevery::Int=1)
