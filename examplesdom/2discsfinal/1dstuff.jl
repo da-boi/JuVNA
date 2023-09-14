df8f8a6efb8f3823e241b57e1ef9d7077ee1d62f
bergermann <dominik.bergermann@gmx.de>
2023-09-14 14:13:20 +0200

    1d stuff

--- examplesdom/2discsfinal/1dstuff.jl
+++ examplesdom/2discsfinal/1dstuff.jl
@@ -1,6 +1,7 @@
 using Dragoon
 using Plots
 using BoostFractor
+import Dragoon: move
 
 function groupDelay(ref::Vector{ComplexF64},freqs::Vector{Float64})
     if length(ref) != length(freqs)
@@ -27,7 +28,8 @@ ObjRefRef(ref,ref0) = Callback(objRef,(ref,ref0))
 
 
 
-p0 = [0.013,0.027]
+p0 = [0.013,0.027]*1.114
+
 
 
 
@@ -36,10 +38,10 @@ b = AnalyticalBooster(p0,2,1e-3,9.4,1e-3,1e-4,0.2,
     Dragoon.unow(),Dragoon.unow(),0)
 
 
-Dragoon.move(b,p0; additive=false)
+move(b,p0; additive=false)
 
 ref0 = getRef1d(b,freqs)
-Obj = ObjRefRef(getRef1d(b,freqs),ref0)
+Obj = ObjRefLin(ref0)
 ObjR(ref) = ObjRefRef(ref,ref0)
 
 steps = 200
@@ -52,7 +54,7 @@ R = zeros(ComplexF64,2*steps+1,2*steps+1,128)
 @time for i in -steps:steps, j in -steps:steps
     j == -steps && println("i: $i")
 
-    Dragoon.move(b,p0+[i*0.00001,j*0.00001]; additive=false)
+    move(b,p0+[i*0.00001,j*0.00001]; additive=false)
     
     ref = getRef1d(b,freqs)
     updateHist!(b,histscan,freqs,ObjR(ref))
@@ -64,32 +66,55 @@ R_ = reverse(reshape((x->x.objvalue).(histscan[1:end-1]),
 
 scan = contourf(-2:2/steps:2,-2:2/steps:2,log10.(R_);
     color=:turbo,levels=30,lw=0,aspect_ratio=:equal,
-    clim=(-1,2.4))
+    clim=(-1,2.3))
 
 xlims!((-2,2)); ylims!((-2,2))
 
 
-import Dragoon: move
 
 move(b,p0; additive=false)
 
 ref0 = getRef1d(b,freqs)
-Obj = ObjRefRef(getRef1d(b,freqs),ref0)
+Obj = ObjRefLin(ref0)
 ObjR(ref) = ObjRefRef(ref,ref0)
 
-histlsn = initHist(b,(2*steps+1)^2+1,freqs,Obj);
+histlsn = initHist(b,100001,freqs,Obj);
 updateHist!(b,histlsn,freqs,Obj)
 
-move(b,[0.0,0.001]; additive=true)
+move(b,[0.0004,0.0004]; additive=true)
 
-@time tracelsn = linesearch(b,histlsn,freqs,10e-6,
+@time tracelsn = linesearch(b,histlsn,freqs,-1e-6,
     Obj,
     SolverNewton("inv"),
-    Derivator2(5e-6,10e-6,"double"),
+    Derivator2(1e-6,1e-6,"double"),
     StepNorm("unit"),
-    # SearchExtendedSteps(20),
-    SearchStandard(0,50),
+    SearchExtendedSteps(100),
+    # SearchStandard(0,100),
     UnstuckRandom(1e-5,1);
-    ϵgrad=0.,maxiter=Int(5),showtrace=true,
+    ϵgrad=0.,maxiter=Int(100),showtrace=true,
     resettimer=true);
 
+plotPath(scan,histlsn,p0)
+
+analyse(histlsn,tracelsn,freqs)
+
+
+
+
+function plotPath(p,hist,p0; l=2,u=1e-3)
+    idx = findfirst(x->x.objvalue==0,hist)
+    if isnothing(idx); idx = length(hist); else idx -= 1; end
+
+    X = zeros(idx,2)
+
+    for i in 1:idx
+        X[i,:] = (hist[i].pos-p0)/u
+    end
+
+    p_ = plot!(deepcopy(p),X[:,1],X[:,2];
+        c=:black,linewidth=0.5,legend=false)
+    # xlim!([-l,l])
+    # ylim!([-l,l])
+
+    return p_
+end
